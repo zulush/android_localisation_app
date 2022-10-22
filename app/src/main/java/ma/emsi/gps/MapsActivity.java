3360f4a00b608408e6807f1bc7a3671e19b165f0
Zouizza <szouizza@gmail.com>
2022-10-22 22:19:34 +0100

    1st commit

---
+++ app/src/main/java/ma/emsi/gps/MapsActivity.java
@@ -0,0 +1,226 @@
+package ma.emsi.gps;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import ma.emsi.gps.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    String showUrl = "http://192.168.56.1/localisation/showPositions.php";
+    String insertUrl = "http://192.168.56.1/localisation/createPosition.php";
+    RequestQueue requestQueue;
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private float accuracy;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Add a marker in Sydney and move the camera
+        LatLng maroc = new LatLng(34, -5);
+        //mMap.addMarker(new MarkerOptions().position(maroc).title("Marker in Morocco"));
+        setUpMap();
+        Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_SHORT).show();
+        boolean permissionGranted = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 150, new
+                LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.d("Location:", "Location Changed");
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        altitude = location.getAltitude();
+                        accuracy = location.getAccuracy();
+                        @SuppressLint("StringFormatMatches") String msg = String.format(
+                                getResources().getString(R.string.new_location), latitude,
+                                longitude, altitude, accuracy);
+                        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Marker"));
+                        addPosition(latitude, longitude);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                        String newStatus = "";
+                        switch (status) {
+                            case LocationProvider.OUT_OF_SERVICE:
+                                newStatus = "OUT_OF_SERVICE";
+                                break;
+                            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                                newStatus = "TEMPORARILY_UNAVAILABLE";
+                                break;
+                            case LocationProvider.AVAILABLE:
+                                newStatus = "AVAILABLE";
+                                break;
+                        }
+                        String msg = String.format(getResources().getString(R.string.provider_new_status),
+                                provider, newStatus);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+                        String msg = String.format(getResources().getString(R.string.provider_enabled),
+                                provider);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                        String msg = String.format(getResources().getString(R.string.provider_disabled),
+                                provider);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(maroc));
+
+
+
+}
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new
+                                Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+    private void setUpMap() {
+        Log.d("Resquest", "Sending Request");
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, showUrl,null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Getting Response", response.toString());
+                try {
+                    JSONArray positions = response.getJSONArray("positions");
+                    Log.d("Map", "Adding Markers");
+                    for (int i = 0; i < positions.length(); i++) {
+                        JSONObject position = positions.getJSONObject(i);
+                        mMap.addMarker(new MarkerOptions().position(new
+                                LatLng(position.getDouble("latitude"),
+                                position.getDouble("longitude"))).title("Marker"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Resquest Error",error.toString());
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    void addPosition(final double lat, final double lon) {
+        StringRequest request = new StringRequest(Request.Method.POST,
+                insertUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                TelephonyManager telephonyManager =
+                        (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+                HashMap<String, String> params = new HashMap<String, String>();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                params.put("latitude", lat + "");
+                params.put("longitude", lon + "");
+                params.put("date", sdf.format(new Date()) + "");
+                String imei = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+                params.put("imei", imei);
+                return params;
+            }
+        };
+        requestQueue.add(request);
+    }
+    }
